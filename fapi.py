ec861ee2f29f2514d99a2cc526a4e672e7170af3
promethean2020 <msaleembhatti@gmail.com>
2025-04-14 16:55:21 +0500

    fastapi new feature added.

--- fapi.py
+++ fapi.py
@@ -1,12 +1,20 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+from pydantic import BaseModel, ValidationError
+class User(BaseModel):
+    id: int
+    name: str
+
+# Valid data
+user_list= [User(id= 0, name= "Alice"),User(id= 1, name= "Bob")]
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/user/{user_id}")
+def read_item(user_id: int):
+    user=user_list[user_id]
+    return user
