350dcf549e58710c5f7cd3de2889414002cccb6e
promethean2020 <msaleembhatti@gmail.com>
2025-04-14 23:21:39 +0500

    openai agent in working

--- agent.py
+++ agent.py
@@ -1,12 +1,17 @@
-from agents import Agent, ModelSettings, function_tool
+import asyncio
+import os
+from agents import Agent, ModelSettings, function_tool, Runner
+from dotenv import load_dotenv
+load_dotenv()
+if not os.getenv("OPENAI_API_KEY"):
+    raise ValueError("OPENAI_API_KEY not found in .env file")
+async def main():
+    agent = Agent(
+        name="Math Tutor",
+        instructions="You provide help with math problems. Explain your reasoning at each step and include examples",
+    )
+    result = await Runner.run(agent,"what is algebra.",)
+    print(result.final_output)
 
-@function_tool
-def get_weather(city: str) -> str:
-    return f"The weather in {city} is sunny"
-
-agent = Agent(
-    name="Haiku agent",
-    instructions="Always respond in haiku form",
-    model="o3-mini",
-    tools=[get_weather],
-)
+if __name__ == "__main__":
+    asyncio.run(main())
